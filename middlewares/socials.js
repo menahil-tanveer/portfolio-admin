8d5c6aea3a1bffd630c49e4aee89f82843743285
Menahil <99386372+menahil-tanveer@users.noreply.github.com>
2022-04-09 00:29:22 +0500

    middleware comments

--- middlewares/socials.js
+++ middlewares/socials.js
@@ -2,7 +2,7 @@
  *
  * Author: Menahil
  * Created: 09-04-2022
- * Purpose: This file contains validation logic for education routes (create/update)
+ * Purpose: This file contains validation logic for socials routes (create/update)
  *
  */
 const Joi = require("joi");
@@ -10,7 +10,7 @@ const Joi = require("joi");
  * @param req
  * @param res
  * @param next
- * @description This method is responsible for validating user input at the time education resource creation
+ * @description This method is responsible for validating social account
  */
 function validateSocial(req, res, next) {
   let { user_id, name, url } = req.body;
